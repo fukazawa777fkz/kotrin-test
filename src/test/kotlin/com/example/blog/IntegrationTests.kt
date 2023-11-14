d630c8d7d21e675de3dc047cf86985fc6cad178c
m.fukazawa <mfukazawa@digitalvision.co.jp>
2023-11-14 17:47:00 +0900

    IntegrationTestの注意点をコメントで追記

--- src/test/kotlin/com/example/blog/IntegrationTests.kt
+++ src/test/kotlin/com/example/blog/IntegrationTests.kt
@@ -7,6 +7,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.client.getForEntity
 import org.springframework.http.HttpStatus
 
+// classes = [BlogApplication::class]が無いと失敗する！！！
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = [BlogApplication::class])
 class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
 
