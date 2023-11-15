065fd4344d44b2b958523db46dfb324d468e0a98
m.fukazawa <mfukazawa@digitalvision.co.jp>
2023-11-15 17:32:09 +0900

    IntegrationTest追加

--- src/main/kotlin/com/example/blog/HtmlController.kt
+++ src/main/kotlin/com/example/blog/HtmlController.kt
@@ -10,6 +10,12 @@ class HtmlController {
 
     @GetMapping("/")
     fun blog(model: Model): String {
+        // model.addAttribute("title", "Blog")
+        // ではなく
+        // model["title"] = "Blog" を記述できるように、
+        // org.springframework.ui.set 拡張機能をインポートします
+        // https://docs.spring.io/spring-framework/docs/current/kdoc-api/
+        // https://docs.spring.io/spring-framework/docs/current/kdoc-api/spring-context/org.springframework.ui/set.html
         model["title"] = "Blog"
         return "blog"
     }
